56375b87e06318d74e63e877322dcb713bc0a280
Nick Vella <nick@nxk.me>
2015-09-16 22:09:04 +1000

    fix path typo

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var tgateway = new (require('./lib/tgateway'))(JSON.parse(fs.readFileSync('confi
 
 // Load frontend drivers
 // Telnet frontend, allows clients to connect to the server via telnet
-tgateway.loadFrontendDriver('telnet', require('./lib/tgateway/frontends/telnet'));
+tgateway.loadFrontendDriver('telnet', require('./lib/frontends/telnet'));
 
 tgateway.run();
