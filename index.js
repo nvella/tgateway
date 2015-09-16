d0e3cc886f8fd914c2b498bac81e3506f581f9d8
Nick Vella <nick@nxk.me>
2015-09-16 20:17:32 +1000

    Basic framework for drivers

--- index.js
+++ index.js
@@ -3,4 +3,8 @@
 var fs = require('fs');
 var tgateway = new (require('./lib/tgateway'))(JSON.parse(fs.readFileSync('config.json')));
 
+// Load frontend drivers
+// Telnet frontend, allows clients to connect to the server via telnet
+tgateway.loadFrontendDriver('telnet', require('./lib/tgateway/frontends/telnet'));
+
 tgateway.run();
