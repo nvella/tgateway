83ee9c55f9ca9b21658c6f82ac45273a63c67fb6
Nick Vella <nick@nxk.me>
2015-09-19 01:02:01 +1000

    load telnet backend driver

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ var tgateway = new (require('./lib/tgateway'))(JSON.parse(fs.readFileSync('confi
 // Telnet frontend, allows clients to connect to the server via telnet
 tgateway.loadFrontendDriver('telnet', require('./lib/frontends/telnet'));
 
+// Load backend drivers
+// Telnet backend, allows tgateway to connect to telnet servers
+tgateway.loadBackendDriver('telnet', require('./lib/backends/telnet'));
+
 tgateway.run();
